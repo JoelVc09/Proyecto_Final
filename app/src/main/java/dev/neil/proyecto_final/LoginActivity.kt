8d9c3897078ebdb15db92023ac91970b0d336b98
renzomontenegro <renzomogra@gmail.com>
2024-06-20 19:15:38 -0500

    Pantalla Inicio, Login, Registro como Usuario y conexión a Firebase completada.

--- app/src/main/java/dev/neil/proyecto_final/LoginActivity.kt
+++ app/src/main/java/dev/neil/proyecto_final/LoginActivity.kt
@@ -1,10 +1,17 @@
 package dev.neil.proyecto_final
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import dev.neil.proyecto_final.Cliente.RegistroClienteActivity
+import dev.neil.proyecto_final.Empresa.EmpresaHistorialActividadesFragment
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,45 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val etLoginEmail: EditText = findViewById(R.id.etLoginEmail)
+        val etLoginContrasenia: EditText = findViewById(R.id.etLoginContrasenia)
+        val btnIngresar: Button = findViewById(R.id.btnIngresar)
+        val btnRegistrarUsuario: Button = findViewById(R.id.btnRegistrarUsuario)
+        val btnRegistrarEmpresa: Button = findViewById(R.id.btnRegistrarEmpresa)
+        val auth = FirebaseAuth.getInstance()
+
+        btnRegistrarUsuario.setOnClickListener {
+            startActivity(
+                Intent(this
+                , RegistroClienteActivity::class.java)
+            )
+        }
+
+        btnIngresar.setOnClickListener {
+            val email = etLoginEmail.text.toString()
+            val password = etLoginContrasenia.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Inicio de sesión exitoso"
+                            , Snackbar.LENGTH_LONG
+                        ).show()
+                        startActivity(
+                            Intent(this
+                            , EmpresaHistorialActividadesFragment::class.java)
+                        )
+                    }else{
+                        Snackbar.make(
+                            findViewById(android.R.id.content)
+                            , "Credenciales inválidas"
+                            , Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+
     }
 }
---
+++ app/src/main/java/dev/neil/proyecto_final/model/ClienteModel.kt
@@ -0,0 +1,8 @@
+package dev.neil.proyecto_final.model
+
+data class ClienteModel(
+    val email: String = "",
+    val fullName: String = "",
+    val numeroCelular: String = "",
+    val UID: String? = ""
+)
